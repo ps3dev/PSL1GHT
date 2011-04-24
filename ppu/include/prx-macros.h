e367a51916cc0f13f9e7b85daa6c6d668e079950
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2011-04-24 04:52:28 -0400

    Adding basic macros for pointer/u32 conversion

---
+++ ppu/include/prx-macros.h
@@ -0,0 +1,11 @@
+#ifndef __PRX_MACROS_H__
+#define __PRX_MACROS_H__
+
+#include <ppu-types.h>
+
+#define PTR(x) ((u32) ((u64) x))
+#define CHAR_PTR(x) ((char *) ((u64) x))
+#define VOID_PTR(x) ((void *) ((u64) x))
+
+
+#endif /* __PRX_MACROS_H__ */
