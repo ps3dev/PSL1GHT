7a901c6baec06cd59a3d08d3ffa478d10e221d76
unknown <nippurct@gmail.com>
2020-06-20 00:13:48 +0200

    Added utime and sysFsUtime

--- ppu/librt/utime.c
+++ ppu/librt/utime.c
@@ -10,19 +10,26 @@
 #include <sys/file.h>
 #include <utime.h>
 
+
+int _utime(const char *path, const struct utimbuf *times, int* _errno)
+{
+	printf("CIAO\n");
+if (times == NULL) {
+		struct utimbuf now_time;
+		time_t     now;
+		now = time(NULL);
+		now_time.actime = now;
+		now_time.modtime = now;
+		return lv2errno(sysLv2FsUtime(path,(sysFSUtimbuf *)&now_time));
+	}
+	else return lv2errno(sysLv2FsUtime(path,(sysFSUtimbuf *)times));
+}
+
 int
 _DEFUN(__librt_utime_r,(r,path,times),
 	   struct _reent *r _AND
 	   const char *path _AND
 	   const struct utimbuf *times)
 {
-	if (times == NULL) {
-		struct utimbuf now_time;
-		time_t     now;
-		now = time(NULL);
-		now_time.actime = now;
-		now_time.modtime = now;
-		return lv2errno_r(sysLv2FsUtime(path,(sysFSUtimbuf *)&now_time));
-	}
-	else return lv2errno_r(sysLv2FsUtime(path,(sysFSUtimbuf *)times));
+	return _utime(path,times,&r->_errno);
 }
