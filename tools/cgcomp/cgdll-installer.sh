57b22523cabf3fad4f29895d97d207a2c66c9945
crystalct <nippurct@gmail.com>
2020-06-30 08:26:54 +0200

    cgdll-install.sh just for Win users
    It is called from make install and only when user is a MinGW or CGYWIN users.

--- tools/cgcomp/cgdll-installer.sh
+++ tools/cgcomp/cgdll-installer.sh
@@ -1,15 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/sh -e
 # NVIDIA cg.dll installer script by CrystalCT (crystal@unict.it)
 
 ## Uname string
 UNAME=$(uname -a)
 
-if [[ "$UNAME" =~ (CYGWIN|MINGW) ]];
-then 
+ 
 	## Download a fresh cg.dll
 	rm -f cg.zip;
 	rm -f cg.dll;
-	if [[ "$UNAME" =~ (x86_64|MINGW64) ]];
+	if [[ $UNAME =~ "x86_64" || $UNAME =~ "MINGW64" ]];
 	then
 		wget --continue --no-check-certificate https://wikidll.com/download/3726/cg.zip;
 	else
@@ -20,12 +19,12 @@ then
 	echo "Installing cg.dll in $PS3DEV/bin"
 	unzip cg.zip
 	rm -f cg.zip
-	CGHASH=$(rhash -C cg.dll -p "%{crc32}") 
-	if [[ "$CGHASH" =~ (28f6073b|e5228ec2) ]];
+	CGHASH=$(rhash -C cg.dll -p "%{crc32}")
+	## CRC HASH values taken from dll inside original NVIDIA CG Tool Kit. Dll can't be downloaded directly from NIVIDA web site.
+	if [[ $CGHASH =~ "28f6073b" || $CGHASH =~ "e5228ec2" ]];
 	then
 		chmod 766 cg.dll
 		mv -f cg.dll $PS3DEV/bin;
 	else
 		echo "Error - cg.dll: wrong CRC32";
-	fi;
-fi
+	fi
